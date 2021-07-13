8c1fae3e128b6046e27ac23dc19c01d3de515b31
DaniBlue10 <86781214+DaniBlue10@users.noreply.github.com>
2021-07-13 13:32:58 +0200

    1.0.0

--- app/src/main/java/com/example/gordeo/FoodPornActivity.java
+++ app/src/main/java/com/example/gordeo/FoodPornActivity.java
@@ -2,13 +2,24 @@ package com.example.gordeo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FoodPornActivity extends AppCompatActivity {
+
+    private Button btnmenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_porn);
+
+        btnmenu = (Button)findViewById(R.id.buttonmenu);
+    }
+    public void mostrarMenu(View v){
+        Intent x = new Intent (this,MainActivity.class);
+        startActivity(x);
     }
 }
--- app/src/main/java/com/example/gordeo/MainActivity.java
+++ app/src/main/java/com/example/gordeo/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.gordeo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnFoodPorn,btnRest,btnYa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFoodPorn =(Button)findViewById(R.id.btn);
+        View.OnClickListener fd = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             mostrarFoodPorn();
+            }
+        };
+        btnFoodPorn.setOnClickListener(fd);
+        btnRest = (Button)findViewById(R.id.btn2);
+        View.OnClickListener CL = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostraRestaurante();
+            }
+
+
+        };
+        btnRest.setOnClickListener(CL);
+        btnYa = (Button)findViewById(R.id.btn3);
+
+
+
+    }
+    public void mostrarFoodPorn (){
+        Intent i = new Intent(this,FoodPornActivity.class);
+        startActivity(i);
+    }
+    public  void mostraRestaurante() {
+        Intent i = new Intent(this,Rest_Activity.class);
+        startActivity(i);
     }
 }

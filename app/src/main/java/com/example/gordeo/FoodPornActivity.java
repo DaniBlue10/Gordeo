2eb0eb3387e9b58386c994403b6403b1e8ab047f
DaniBlue10 <86781214+DaniBlue10@users.noreply.github.com>
2021-07-14 14:38:50 +0200

    act 1.0.1

--- app/src/main/java/com/example/gordeo/FoodPornActivity.java
+++ app/src/main/java/com/example/gordeo/FoodPornActivity.java
@@ -6,20 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FoodPornActivity extends AppCompatActivity {
 
-    private Button btnmenu;
+    private ImageButton botonPizza,botonHamburguesa,botonCostillas,botonNachos,
+    botonSushi,botonPerrito,botonTaco,botonSandwich,botonPadthai,botonArroz,botonTarta,
+    botonHelado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_porn);
 
-        btnmenu = (Button)findViewById(R.id.buttonmenu);
+        botonPizza = (ImageButton)findViewById(R.id.buttonpizza);
+        botonHamburguesa = (ImageButton)findViewById(R.id.buttonHamburguesa);
+        botonCostillas = (ImageButton)findViewById(R.id.buttonCostillas);
+        botonNachos = (ImageButton)findViewById(R.id.buttonNachos);
+        botonSushi = (ImageButton)findViewById(R.id.buttonSushi);
+        botonPerrito = (ImageButton)findViewById(R.id.buttonPerrito);
+        botonTaco = (ImageButton)findViewById(R.id.buttonTaco);
+        botonSandwich = (ImageButton)findViewById(R.id.buttonSandwich);
+        botonPadthai = (ImageButton)findViewById(R.id.buttonPadthai);
+        botonArroz = (ImageButton)findViewById(R.id.buttonArroz);
+        botonTarta = (ImageButton)findViewById(R.id.buttonTarta);
+        botonHelado = (ImageButton)findViewById(R.id.buttonHeladoyGofre);
+
+
     }
-    public void mostrarMenu(View v){
-        Intent x = new Intent (this,MainActivity.class);
-        startActivity(x);
-    }
+
 }
